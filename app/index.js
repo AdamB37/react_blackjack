e7ed2d632b7fc1e270b8c473a99128aa99476cbe
AdamB37 <adam.beshir@gmail.com>
2017-04-04 16:03:53 -0700

    second commit

--- app/index.js
+++ app/index.js
@@ -3,7 +3,25 @@ import ReactDOM from 'react-dom'
 
 import App from './components/app'
 
+import { newDeck } from './lib/cards'
+
+let deck = newDeck()
+console.log('start deck')
+console.log(deck)
+
+// let playerHand = deck.takeLast(2)
+// deck = deck.skipLast(2)
+// let dealerHand = deck.takeLast(2)
+// deck = deck.skipLast(2)
+//
+// console.log('end deck: ')
+// console.log(deck)
+// console.log('player hand: ')
+// console.log(playerHand)
+// console.log('dealer hand: ')
+// console.log(dealerHand)
+
 ReactDOM.render(
   <App />,
-  document.getElementByID('app')
+  document.getElementById('app')
 );
